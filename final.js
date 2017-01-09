c7235b8f4994e2077fc2dec6ddcaa67532164f54
nikki <zhenya_nikitenko@ukr.net>
2017-01-09 22:53:35 +0200

    applying changes

--- final.js
+++ final.js
@@ -1,92 +1,70 @@
 var game = {};
-game.array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'hole'];
+game.array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 $(document).ready(function() {
     game.array = shuffle(game.array);
+    game.field = $('.gamefield');
 
     createField(game.array);
-    var items = $('.move');
-    items.on('click swipe', function(event) {
 
-        var index = $(this).index();
-        var holePos = game.array.indexOf('hole')
-        makeTurn(game.array, $(this).index());
+    game.items = $('.move');
 
-        // анимация
-        if ((index == holePos - 1) && (index % 4 !== 3)) {
-            $(this).css("z-index", "100").animate({
-                right: '-=110px'
-            }, 900).animate({
-                right: '+=110px'
-            }, 0);
-        } else if ((index == holePos + 1) && (index % 4 !== 0)) {
-            $(this).css("z-index", "100").animate({
-                right: '+=110px'
-            }, 900).animate({
-                right: '-=110px'
-            }, 0);
-        } else if (index == holePos - 4) {
-            $(this).css("z-index", "100").animate({
-                top: '+=110px'
-            }, 900).animate({
-                top: '-=110px'
-            }, 0);
-        } else if (index == holePos + 4) {
-            $(this).css("z-index", "100").animate({
-                top: '-=110px'
-            }, 900).animate({
-                top: '+=110px'
-            }, 0);
+    game.items.on('click swipe', function(event) {
+        var self = $(this);
+        var hole = $('.hole');
+        game.index = self.index();
+        game.holePos = game.array.indexOf(0);
+        
+        turn(game.array, game.index, game.holePos);
+
+        if (chechWin(game.array)) {
+            alert('win');
+            location.reload();
         }
-
-        $(this).delay(0).queue(function(next) {
-            $(this).css("z-index", "10");
-            reflectTurn();
-            if (chechWin(game.array)) {
-                alert('you win')
-            }
-            next();
-        });
     })
 })
 
-// отображаем изменения в массиве игры
-function reflectTurn() {
-    var items = $('.move');
-    for (var i = 0; i < items.length; i++) {
-        items[i].innerHTML = game.array[i];
-    }
-    for (var i = 0; i < items.length; i++) {
-        if (items[i].innerHTML == 'hole') {
-            items[i].classList.add('hole')
-        } else {
-            items[i].classList.remove('hole')
-        }
+// меняем местами елементы массива игры
+function swap(arr, x, y) {
+    var tmp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = tmp;
+    return arr;
+}
+
+// делаем ход и отображаем его
+function turn(arr, index, hole) {
+    if (isPossibleToTurn(index, hole)) {
+        game.items[hole].innerHTML = game.items[index].innerHTML;
+        game.items[hole].classList.remove('hole');
+        game.items[index].innerHTML = 0;
+        game.items[index].classList.add('hole');
+        return swap(arr, index, hole);
     }
 }
 
 // создаем игровое поле
 function createField(array) {
+    game.field.text('');
     for (var k = 0; k < 16; k++) {
-        var item = $('<div></div>'); // создаем пустой div
-        if (typeof array[k] == 'number') { // заполняем поле, если элемент масива - число
-            $('.gamefield').append($('<div></div>').addClass('move').text(array[k]));
+        var item = $('<div></div>');
+        if (array[k] !== 0) {
+            game.field.append(item.addClass('move').text(array[k]));
         } else {
-            $('.gamefield').append($('<div></div>').addClass('move hole').text(array[k])); // если слово "дыра"
-            //game.holePos = k;
+            game.field.append(item.addClass('move hole').text(array[k]));
         }
     }
 }
 
 // проверка массива игры на существование решения
-function isUndecidable(arr) {
+function isDecidable(arr) {
     var c = 0;
-    var h = 1 + (arr.indexOf('hole') - (arr.indexOf('hole') % 4)) / 4;
+    var h = 1 + (arr.indexOf(0) - (arr.indexOf(0) % 4)) / 4;
     for (var i = 0; i < arr.length - 1; i++) {
         var elem = arr[i];
-        if (typeof elem == 'number') {
-            for (var j = i + 1; j < arr.length; j++) { //here
-                if (elem > arr[j]) {
+        if (elem > 0) {
+            for (var j = i + 1; j < arr.length; j++) {
+                if (elem > arr[j] && arr[j] !== 0) {
                     c++;
                 }
             }
@@ -100,22 +78,13 @@ function shuffle(arr) {
     var b = true;
     while (b) {
         arr = arr.sort(function() {
-            return 0.5 - Math.random()
+            return 0.5 - Math.random();
         });
-        if (isUndecidable(arr)) {
+        if (isDecidable(arr)) {
             b = false;
         }
     }
     return arr;
-}
-
-// определение начальной позиции пустой ячейки
-function findHolePos(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] == 'hole') {
-            return i;
-        }
-    }
 }
 
 // проверка на возможность хода в ячейку (ход по правилам)
@@ -126,25 +95,6 @@ function isPossibleToTurn(index, holePos) {
         return true;
     } else {
         return false;
-    }
-}
-
-// делаем ход (меняем местами ячейку хода и пустую ячейку)
-function makeTurn(array, index) {
-    var e;
-    var holePos = findHolePos(array)
-    array.forEach(function(elem, i) {
-        if (i == index) {
-            e = elem;
-        }
-    })
-    if (isPossibleToTurn(index, holePos)) {
-        array.forEach(function(elem, i) {
-            if (typeof elem == 'string') {
-                array.splice(i, 1, e);
-            }
-        })
-        return array.splice(index, 1, 'hole');
     }
 }
 
